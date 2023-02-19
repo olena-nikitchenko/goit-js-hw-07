7a1c36e4d72601d6d87fad8e15cd1aadbcef2502
olena-nikitchenko <helnikitka@gmail.com>
2023-02-19 19:58:45 +0200

    add  tag li

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,10 +9,9 @@ containerImages.insertAdjacentHTML('beforeend', cardsMarkup);
 function createImageCardsMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
-            return `
-            <a class="gallery__item" href="${original}">
+            return `<li><a class="gallery__item" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
         })
         .join('');
 }
